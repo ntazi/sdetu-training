043f26f5e0254c80022939b8b93ea12bc314e811
ntazi <65911139+ntazi@users.noreply.github.com>
2020-05-25 17:23:28 +0100

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Adding code to my projet!");
+		System.out.println("Adding code from github to my projet!");
 	}
 
 }
